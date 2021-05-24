4e2fb6a1905e94db28818ed2c523b24a431ab9fa
agray998 <a.gray.4797@gmail.com>
2021-05-24 15:56:21 +0000

    Written apps and dockerfiles

--- animal_api/app.py
+++ animal_api/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify, Response
+import requests
+import random
 app = Flask(__name__)
 
 # animal generator route here
+@app.route('/get_animal', methods = ['GET'])
+def get_animal():
+    dict = {1:"pig", 2:"cow", 3:"horse"}
+    num = random.randint(1, 4)
+    animal = dict[num]
+    return Response(animal, mimetype='text/plain')
 
 # animal noise generator route here
+@app.route('/get_noise', methods = ['POST'])
+def get_noise():
+    dict = {"pig":"oink", "cow":"moo", "horse":"neigh"}
+    animal = request.data.decode("utf-8")
+    noise = dict[animal]
+    return jsonify({"animal":animal, "noise":noise})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- server/app.py
+++ server/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request 
+import requests
 
 app = Flask(__name__)
 
 # home route here
 # must query the animal API for an animal and a noise – the noise should be based on the animal
+@app.route('/home', methods = ['GET', 'POST'])
+def home():
+    if method == 'GET':
+        animal = requests.get('http://api:5000/get_animal')
+        response = requests.post('http://api:5000/get_noise', data = animal)
+        package = response.json()
+        noise = package["noise"]
+        return f"{animal} goes {noise}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
