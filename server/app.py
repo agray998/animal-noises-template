ac4e112374907d7ac9f412e51d0e63e1b18e46f3
agray998 <a.gray.4797@gmail.com>
2021-05-25 07:33:40 +0000

    Removed unnecessary request methods from server app

--- server/app.py
+++ server/app.py
@@ -5,12 +5,11 @@ app = Flask(__name__)
 
 # home route here
 # must query the animal API for an animal and a noise – the noise should be based on the animal
-@app.route('/home', methods = ['GET', 'POST'])
+@app.route('/home', methods = ['GET'])
 def home():
-    if request.method == 'GET':
-        animal = requests.get('http://animal_api:5000/get_animal')
-        noise = requests.post('http://animal_api:5000/get_noise', data = animal.text)
-        return render_template('index.html', animal=animal.text, noise=noise.text)
+    animal = requests.get('http://animal_api:5000/get_animal')
+    noise = requests.post('http://animal_api:5000/get_noise', data = animal.text)
+    return render_template('index.html', animal=animal.text, noise=noise.text)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
